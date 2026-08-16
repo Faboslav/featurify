d3842d7d5d79b45acbe54c3f853a7aa38caf4c04
Faboslav <fabianek.ales@gmail.com>
2026-08-16 14:27:41 +0200

    Optional mixin

--- common/src/main/java/com/faboslav/featurify/common/mixin/compat/terrablender/ParameterListMixin.java
+++ common/src/main/java/com/faboslav/featurify/common/mixin/compat/terrablender/ParameterListMixin.java
@@ -8,22 +8,23 @@ import com.faboslav.featurify.common.worldgen.biome.TerraBlenderBiomeFilter;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
-import org.spongepowered.asm.mixin.Dynamic;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import java.util.function.Consumer;
 
+@Pseudo
 @Mixin(value = Climate.ParameterList.class, priority = 2000, remap = false)
 public abstract class ParameterListMixin
 {
-	@Dynamic("Targets initializeForTerraBlender added by TerraBlender's mixin")
 	@ModifyArg(
 		method = "initializeForTerraBlender",
 		at = @At(
 			value = "INVOKE",
 			target = "Lterrablender/api/Region;addBiomes(Lnet/minecraft/core/Registry;Ljava/util/function/Consumer;)V"
-		)
+		),
+		require = 0
 	)
 	private Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> featurify$filterRegionBiomes(
 		Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper
